2edec59afb9cd1977cb73ecfd077c78be9fb9273
a14main <105232109+a14main@users.noreply.github.com>
2023-10-28 06:30:11 +0000

    Complete caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,29 @@
-const caesar = function() {
-
+const caesar = function(message, shift) {
+  if (shift < 0) {
+    shift = 26 + (shift % 26);
+  }
+  const capA = 'A'.charCodeAt(0);
+  const lowA = 'a'.charCodeAt(0);
+  const arr = message
+    .split('')
+    .map(char => {
+      if (!char.match(/[A-z]/)) {
+        return char;
+      }
+      let code = char.charCodeAt(0);
+      if (char.match(/[A-Z]/)) {
+        code -= capA;
+        code = (code + shift) % 26;
+        code += capA;
+      } else if (char.match(/[a-z]/)) {
+        code -= lowA;
+        code = (code + shift) % 26;
+        code += lowA;
+      }
+      return String.fromCharCode(code);
+    });
+  
+  return arr.join('');
 };
 
 // Do not edit below this line
