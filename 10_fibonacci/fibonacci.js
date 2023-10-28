58ebc84af4f83a725e9fc0fd299e87172b290d65
a14main <105232109+a14main@users.noreply.github.com>
2023-10-28 05:33:06 +0000

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(i,a=1,b=1) {
+  if (i < 1) {
+    return 'OOPS';
+  }
+  if (i < 2) {
+    return a;
+  }
+  return fibonacci(i-1,b,a+b);
 };
 
 // Do not edit below this line
