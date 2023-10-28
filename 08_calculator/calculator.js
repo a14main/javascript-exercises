bed0a6582ba3a64ecc6e6d6000c1c92cfc06dd31
a14main <105232109+a14main@users.noreply.github.com>
2023-10-28 03:43:23 +0000

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a,b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a*b,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n, result=1) {
+  if (n <= 1) {
+    return result;
+  }
+  return factorial(n-1,result * n);
 };
 
 // Do not edit below this line
