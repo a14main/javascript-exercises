905df7c4b4c0eddedc94c2ccd13efb85fe80ca63
a14main <105232109+a14main@users.noreply.github.com>
2023-10-28 05:23:58 +0000

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const arr = str
+    .toLowerCase()
+    .split('')
+    .filter(ch => ch.match(/[a-z]/));
+  const len = arr.length;
+  for (let i = 0; i < len / 2 - 1; i++) {
+    if (arr[i] !== arr[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
